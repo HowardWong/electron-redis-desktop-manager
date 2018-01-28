dc548dea8c18840b6922bfa4ee0ffdca23e60e31
王旭浩 <AlohaWangxh@163.com>
2018-01-28 23:14:21 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const electron = require('electron')
 const env = process.env.NODE_ENV;
+
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
